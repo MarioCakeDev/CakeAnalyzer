856a25482ff44aea46636a20e53b06326c71c29d
MarioCakeDev <davidleinweber@web.de>
2021-02-07 16:33:28 +0100

    Adding an analyzer which checks for simple xml comments by removing all whitespaces and comparing them ignoring its case.

---
+++ Analyzers.Tests/SimpleXmlParamAnalyzerTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using Verify = Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<Analyzers.SimpleXmlParamAnalyzer>;
+
+namespace Analyzers.Tests
+{
+    public class SimpleXmlParamAnalyzerTests
+    {
+        private const string RuleId = SimpleXmlParamAnalyzer.RuleId;
+
+        [Theory]
+        [InlineData("thisIsAParameterName")]
+        [InlineData("this Is A Parameter Name")]
+        [InlineData("THIS IS A PARAMETER NAME")]
+        [InlineData("this is a parameter name")]
+        [InlineData("  this is a parameter name  ")]
+        [InlineData("\tthis is a\tparameter name  ")]
+        [InlineData("thisIsA ParameterName")]
+        public async Task Finds_DiagnosticResult_With_Simple_Xml_Comments(string paramComment)
+        {
+            string test = $@"
+class C 
+{{
+    /// <param name=""thisIsAParameterName"">{paramComment}</param>
+    void M(string thisIsAParameterName){{}}
+}}";
+            const int startColumn = 9;
+            int endColumn = paramComment.Length + 52;
+            DiagnosticResult[] expected =
+            {
+                Verify.Diagnostic(RuleId).WithSpan(4, startColumn, 4, endColumn)
+            };
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Theory]
+        [InlineData("thisIsAParameterName")]
+        [InlineData("this Is A Parameter Name")]
+        [InlineData("THIS IS A PARAMETER NAME")]
+        [InlineData("this is a parameter name")]
+        [InlineData("  this is a parameter name  ")]
+        [InlineData("\tthis is a\tparameter name  ")]
+        [InlineData("thisIsA ParameterName")]
+        public async Task Finds_DiagnosticResult_With_Simple_Multiline_Xml_Comments(string paramComment)
+        {
+            string test = $@"
+class C 
+{{
+    /** <param name=""thisIsAParameterName"">{paramComment}</param> **/
+    void M(string thisIsAParameterName){{}}
+}}";
+            const int startColumn = 9;
+            int endColumn = paramComment.Length + 52;
+            DiagnosticResult[] expected =
+            {
+                Verify.Diagnostic(RuleId).WithSpan(4, startColumn, 4, endColumn)
+            };
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+        
+        [Theory]
+        [InlineData("thisIsAParameterName")]
+        [InlineData("this Is A Parameter Name")]
+        [InlineData("THIS IS A PARAMETER NAME")]
+        [InlineData("this is a parameter name")]
+        [InlineData("  this is a parameter name  ")]
+        [InlineData("\tthis is a\tparameter name  ")]
+        [InlineData("thisIsA ParameterName")]
+        public async Task Finds_DiagnosticResult_With_Simple_Xml_Comments_Over_Multiple_Lines(string paramComment)
+        {
+            string test = $@"
+class C 
+{{
+    /// <param name=""thisIsAParameterName"">
+    /// {paramComment}
+    /// </param>
+    void M(string thisIsAParameterName){{}}
+}}";
+            DiagnosticResult[] expected =
+            {
+                Verify.Diagnostic(RuleId).WithSpan(4, 9, 6, 17)
+            };
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+    }
+}
---
+++ Analyzers/SimpleXmlParamAnalyzer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Analyzers
+{
+    /// <summary>
+    /// Checks whether the param tag contains a parameter which is too simple by comparing that the value of the xml
+    /// tag exactly the parameter name.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SimpleXmlParamAnalyzer : DiagnosticAnalyzer
+    {
+        private const string Title = "Parameter is too simple.";
+
+        private const string MessageFormat =
+            "Param tag in XML comment is not allowed to be a form of the parameter name itself.";
+
+        private const string Description = "Parameter is too simple.";
+        public const string RuleId = "XmlParameterTagTooSimple100";
+
+        private static readonly DiagnosticDescriptor Rule = new(
+            RuleId,
+            Title,
+            MessageFormat,
+            "XmlDocumentation",
+            DiagnosticSeverity.Warning,
+            true,
+            Description
+        );
+
+        /// <inheritdoc />
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+
+        /// <inheritdoc />
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeSimpleXmlComment,
+                SyntaxKind.SingleLineDocumentationCommentTrivia,
+                SyntaxKind.MultiLineDocumentationCommentTrivia
+            );
+        }
+
+        private static void AnalyzeSimpleXmlComment(SyntaxNodeAnalysisContext startCodeBlockContext)
+        {
+            DocumentationCommentTriviaSyntax syntaxNode = (DocumentationCommentTriviaSyntax) startCodeBlockContext.Node;
+
+            IEnumerable<XmlElementSyntax> paramTags = GetParamXmlTags(syntaxNode);
+
+            Regex whitespace = new("\\s", RegexOptions.Multiline);
+            Regex xmlCommentTokens = new("(///|\\*)", RegexOptions.Multiline);
+            foreach (XmlElementSyntax paramTag in paramTags)
+            {
+                string parameterName = (paramTag.StartTag.Attributes.FirstOrDefault(
+                    attribute => attribute is XmlNameAttributeSyntax
+                ) as XmlNameAttributeSyntax)?.Identifier.ToString();
+                if (string.IsNullOrEmpty(parameterName))
+                {
+                    continue;
+                }
+
+                string contentWithoutWhitespace = whitespace.Replace(paramTag.Content.ToString(), "");
+                contentWithoutWhitespace = xmlCommentTokens.Replace(contentWithoutWhitespace, "");
+
+                if (!string.Equals(contentWithoutWhitespace, parameterName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continue;
+                }
+
+                Diagnostic diagnostic = Diagnostic.Create(Rule, paramTag.GetLocation());
+                startCodeBlockContext.ReportDiagnostic(diagnostic);
+            }
+        }
+        
+        private static IEnumerable<XmlElementSyntax> GetParamXmlTags(DocumentationCommentTriviaSyntax xmlDocumentation)
+        {
+            IEnumerable<XmlElementSyntax> xmlElements = xmlDocumentation!.Content.SelectMany(element =>
+            {
+                bool isXmlTag = element is XmlElementSyntax;
+                if (!isXmlTag)
+                {
+                    return new XmlElementSyntax[0];
+                }
+
+                XmlElementSyntax xmlElement = (XmlElementSyntax) element;
+
+                bool isParamTag = xmlElement.StartTag.Name.ToString() == "param";
+                return isParamTag ? new[] {xmlElement} : new XmlElementSyntax[0];
+            });
+            return xmlElements;
+        }
+    }
+}
